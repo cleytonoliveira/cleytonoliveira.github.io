4387d52ff39a5fbd333302db5d8704ade4d857d7
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2024-09-28 22:23:11 -0300

    feat: add recipes app project

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,4 +1,4 @@
-import { BiLogoNodejs, BiLogoReact, BiLogoTypescript, BiLogoCss3, BiLogoJavascript, BiLogoTailwindCss, BiLogoPostgresql, BiLogoDocker } from "react-icons/bi";
+import { BiLogoNodejs, BiLogoReact, BiLogoTypescript, BiLogoCss3, BiLogoJavascript, BiLogoTailwindCss, BiLogoPostgresql, BiLogoDocker, BiLogoHtml5 } from "react-icons/bi";
 import { SiNextdotjs, SiStyledcomponents, SiJest } from "react-icons/si";
 
 import { ProjectCard } from "./ProjectCard";
@@ -7,6 +7,8 @@ import { GrMysql } from "react-icons/gr";
 export function ProjectsList() {
   const skills = {
     "css": { name: "CSS", icon: <BiLogoCss3 size={20} /> },
+    "context-api": { name: "Context API", icon: <BiLogoReact size={20} /> },
+    "html": { name: "HTML", icon: <BiLogoHtml5 size={20} /> },
     "docker": { name: "Docker", icon: <BiLogoDocker size={20} /> },
     "javascript": { name: "JavaScript", icon: <BiLogoJavascript size={20} /> },
     "jest": { name: "Jest", icon: <SiJest size={20} /> },
@@ -15,6 +17,7 @@ export function ProjectsList() {
     "node": { name: "Node.js", icon: <BiLogoNodejs size={20} /> },
     "postgresql": { name: "PostgreSQL", icon: <BiLogoPostgresql size={20} /> },
     "react": { name: "React.js", icon: <BiLogoReact size={20} /> },
+    "react-hooks": { name: "React Hooks", icon: <BiLogoReact size={20} /> },
     "styled-components": { name: "Styled Components", icon: <SiStyledcomponents size={20} /> },
     "tailwind": { name: "Tailwind CSS", icon: <BiLogoTailwindCss size={20} /> },
     "typescript": { name: "TypeScript", icon: <BiLogoTypescript size={20} /> },
@@ -23,10 +26,17 @@ export function ProjectsList() {
     <div className="flex flex-wrap items-center m-auto">
       <ProjectCard
         title="Ranking System - Team Grube"
-        image={"/projects/grube-mockup.png"}
+        image="/projects/grube-mockup.png"
         description="It organizes athletes' performance based on victories, defeats, and competition scores."
         skills={[skills.next, skills.node, skills.react, skills.postgresql, skills.css]}
         link="https://grube-app.vercel.app/"
+      />
+      <ProjectCard
+        title="Recipes App"
+        image="/projects/app-receitas-mockup.png"
+        description="App to search for recipes and save them to your favorites."
+        skills={[skills.react, skills.html, skills.css, skills["context-api"], skills["react-hooks"]]}
+        link="https://cleytonoliveira.github.io/recipes-app/"
       />
     </div>
   );
